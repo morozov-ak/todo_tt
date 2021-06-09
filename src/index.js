0114ceb83fdcd366443bc0f3d96c9c245d004c76
mr.morozov <mr.morozov_@mail.ru>
2021-06-09 21:31:35 +1000

    отключение редакстулкит

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import { rootReducer } from './redux/rootReducer';
 
 const store = createStore(rootReducer,
   compose(applyMiddleware(thunk)
-  ,window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  //,window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   )
   )
 
